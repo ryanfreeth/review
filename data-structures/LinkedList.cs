4ceed8a340ab6ec79f9ad4526b92cb2a64290a78
Ryan Freeth <ryan@ryanfreeth.com>
2017-10-02 21:14:26 -0700

    linked list review

---
+++ data-structures/LinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace data_structures
+{
+    public class LinkedList
+    {
+        public Node Head { get; private set; }
+        
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public Node Traverse(Func<Node, bool> predicate)
+        {
+            var current = Head;
+            Node last = null;
+            while (current != null)
+            {
+                var found = predicate(current);
+                last = current;
+                current = current.Next;
+                if (found) break;
+            }
+            return last;
+        }
+
+        public void Traverse(Action<string> act)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                act(current.Val.ToString());
+                current = current.Next;
+            }
+        }
+
+        public void InsertAt(Node node, int val)
+        {
+            var newNode = new Node(val) {Next = node.Next};
+            node.Next = newNode;
+        }
+
+        public Node Append(int val)
+        {
+            var newNode = new Node(val);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                var last = Traverse(n => n.Next == null);
+                last.Next = newNode;
+            }
+            return newNode;
+        }
+
+        public void Remove(Node node)
+        {
+            if (Head.Equals(node))
+            {
+                Head = Head.Next;
+            }
+            else
+            {
+                var last = Head;
+                var current = Head.Next;
+                while (current != null)
+                {
+                    if (node.Equals(current)) break;
+                    last = current;
+                    current = current.Next;
+                }
+                last.Next = current.Next;
+            }
+        }
+    }
+
+    public class Node
+    {
+        public Node Next { get; set; }
+        public int Val { get; }
+        
+        public Node(int value)
+        {
+            Val = value;
+        }
+
+        protected bool Equals(Node other)
+        {
+            return Val == other.Val;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Node) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Val;
+        }
+    }
+}
---
+++ data-structures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList();
+            void Print() => list.Traverse(n => WriteLine(n));
+            list.Append(1);
+            var two = list.Append(2);
+            list.Append(3);
+            Print();
+            WriteLine("Inserting 5...");
+            list.InsertAt(two, 5);
+            Print();
+            WriteLine("Removing 2...");
+            list.Remove(two);
+            Print();
+        }
+    }
+}
